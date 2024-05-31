5b5c72064fecedab0e8cad223504ab5ed54a9adb
Niic Silva <168043163+Nicoli-Silva@users.noreply.github.com>
2024-05-31 18:16:10 -0300

    Add files via upload

---
+++ atividade2/atividade2.js
@@ -0,0 +1,11 @@
+const valor = 10;
+
+if(valor > 10 || valor < 0 ) {
+console.log("Esse numero é maior que 10");
+}
+if (valor < 10 ){
+    console.log("Esse numero é menor que 10");
+}
+if(valor === 10){
+    console.log("Esse numero é 10");
+}
